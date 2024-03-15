366ca0190a8285d245bc25be8cd9db203d9943ee
decolector <cmart@decolector.net>
2024-03-15 16:11:12 -0500

    leaflet básico

--- app.js
+++ app.js
@@ -13,17 +13,29 @@ console.log("Hola Mundo")
  //Esta función manupula el DOM
  //creando elementos nuevos en el documentto
  //e instertando datos en esos elementos
+
+
+
  function insertarDatos(datos){
+ 
+  var map = L.map('mapa').setView([4.5963704, -74.0760387], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+ 
   let main = document.getElementById("principal")
   //esta es una iteración sobre los datos
   //obtendos por fetch
   datos.forEach(dato => {
-    //crea un elemento img
-    let imagen = document.createElement("img")
-    //inserta la url de la imagen en el atributo src
-    imagen.setAttribute("src", dato.archivo)
-    //inserta la imagen en el documento
-    main.append(imagen)
+
+    //crea un marcador por cada elemento de los datos
+    //usando las propiedades latitude y longitude
+    L.marker([dato.latitude,dato.longitude]).addTo(map)
+    .bindPopup(dato.titulo)
+    .openPopup();
+
   });
 }
 
